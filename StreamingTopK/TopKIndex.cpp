2661bb5b73292b6f42b5b453ce3366029c38978d
Sicong Liu <sicongliu@sicongs-mbp.mobile.asu.edu>
2017-10-20 17:36:51 -0700

    first commit

---
+++ StreamingTopK/TopKIndex.cpp
@@ -0,0 +1,11 @@
+//
+//  TopKIndex.cpp
+//  StreamingTopK
+//
+//  Created by Sicong Liu on 10/20/17.
+//  Copyright © 2017 Sicong Liu. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
