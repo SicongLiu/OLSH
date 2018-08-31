e85e7a8b854cdbcee4d5ad52384c1d8fe64689ef
Sicong Liu (Student) <sliu104@asu.edu>
2018-08-30 21:22:28 -0700

    Updated Synthetic Data Gen, Task Done.

--- Python_Analysis/Plot_Data_Gen_Test.py
+++ Python_Analysis/Plot_Data_Gen_Test.py
@@ -5,7 +5,7 @@ random_file = "random_data.txt"
 correlated_file = "correlated_data.txt"
 anti_correlated_file = "anticorrelated_data.txt"
 
-data = np.loadtxt(correlated_file)
+data = np.loadtxt(anti_correlated_file)
 plt.plot(data[:, 0], data[:, 1], 'o')
 # fig = plt.figure()
 # ax = plt.subplot(111)
