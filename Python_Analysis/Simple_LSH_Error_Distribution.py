8cfa71fdea63639d03a026c9830155347207f7c1
Sicong Liu (Student) <sliu104@asu.edu>
2018-09-12 16:13:11 -0700

    Fixed hash collision bugs, added python code to demonstrate similarity against success_probability

---
+++ Python_Analysis/Simple_LSH_Error_Distribution.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def compute_s_distribution(successProbability):
+    resultset = []
+    for i in range(len(successProbability)):
+        s = np.cos(np.pi * (1 - successProbability[i]))
+        resultset.append(s)
+    return resultset
+
+
+def compute_success_distribution(s):
+    return 1-np.arccos(s)/np.pi
+
+
+def plot_collision_sim(successProbability, sims):
+    plt.plot(successProbability, sims)
+    plt.title('similarity vs success probability')
+    plt.xlabel('success probability')
+    plt.ylabel('similarity')
+    plt.show()
+    # fig.savefig('plot.png')
+
+
+if __name__ == '__main__':
+    # successProbability_ = [0.3, 0.5, 0.6, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
+    #
+    # successProbability_ = [0.2, 0.9]
+
+    # successProbability_ = [0.59698668, 0.66666667, 0.74681669, 0.85643371, 0.95494659, 0.98576356]
+    successProbability_ = [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.97, 0.98, 0.99]
+    similarity_ = compute_s_distribution(successProbability_)
+    print("similarity: ", similarity_)
+
+    plot_collision_sim(successProbability_, similarity_)
+    # similarity = [0.3, 0.5, 0.7, 0.9, 0.99, 0.999]
+    # successProbability = compute_success_distribution(similarity)
+    # print("success probability: ", successProbability)
--- Python_Analysis/Test.py
+++ Python_Analysis/Test.py
@@ -5,25 +5,7 @@ Created on Feb 20, 2018
 '''
 
 import numpy as np
-import math
-import matplotlib.mlab as mlab
-import matplotlib.pyplot as plt
-from operator import itemgetter
+from math import pi
 
-a = [1, 4, 5, 6]
-b = [8, 1, 2, 3]
-c = [1, 4, 5, 6]
-d = [1, 2, 3, 4]
-# tt = [np.asarray(a), np.array(b), np.array(c)]
-tt = [a, b, c]
-print (tt.index([1, 4, 5, 6]))
-print(a in tt)
-print(d in tt)
-
-print(np.isin(tt, a))
-print(type(tt))
-# tt = np.asarray(tt)
-aa = [1, 4, 5, 6]
-aa = np.asarray(aa)
-index = np.where(tt == aa)
-print (index)
+print(pi)
+print(np.cos(0))
