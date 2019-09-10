b6e68120fb57304eb64e1e031ec1fcaa2c01905c
Sicong Liu (Student) <sliu104@asu.edu>
2019-09-10 09:41:03 -0700

    Python analysis script test

---
+++ Python_Analysis/Norm_Analysis_Test.py
@@ -0,0 +1,185 @@
+import os
+import re
+import sys
+import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+from collections import Counter
+
+# chunks = 25
+# top_k = 25
+# data_folder = '/Users/sicongliu/Desktop/Skyline_qhull_data/'
+# data_type = ['anti_correlated_', 'correlated_', 'random_']
+# dimension = 4
+# cardinality = 200000
+
+# for i in range(data_type.__len__()):
+#     for j in range(25):
+#         total_norm = 0
+#         file_name = data_folder + data_type[i] + str(dimension) + '_' + str(cardinality) + '_qhull_layer_' + str(j)
+#         # print("file name: " + file_name)
+#         f = open(file_name, 'r')
+#         lines = f.readlines()
+#
+#         cur_dim = int(lines[0])
+#         cur_card = int(lines[1])
+#         norm_list = []
+#         for kk in range(cur_card):
+#             current_data_record = np.fromstring(lines[kk + 2], dtype=float, sep=' ')
+#             current_data_record = np.asarray(current_data_record)
+#             temp_norm = np.linalg.norm(current_data_record)
+#             norm_list.append(temp_norm)
+#         f.close()
+#
+#         print('data type: ' + data_type[i] + ', cur_layer: ' + str(j + 1) + ', total # of data: ' + str(cur_card) + ', average norm: ' + str(np.mean(norm_list)) + ', min norm: '
+#               + str(np.min(norm_list)) + ', max_norm: ' + str(np.max(norm_list)) + ', std: ' + str(np.std(norm_list)))
+
+
+def dot(K, L):
+   if len(K) != len(L):
+      return 0
+
+   return sum(i[0] * i[1] for i in zip(K, L))
+
+
+chunks = 25
+top_k = 25
+query_num = 100
+data_folder = '/Users/sliu104/Desktop/StreamingTopK/H2_ALSH/qhull_data/Synthetic/'
+query_folder = '~/Desktop/StreamingTopK/H2_ALSH/query/'
+# data_type = ['anti_correlated_', 'correlated_', 'random_']
+data_type = 'random_'
+dimension = 100
+cardinality = 100000
+layer_index = 0
+bins = 25
+
+
+file_name = data_folder + data_type + str(dimension) + '_' + str(cardinality) + '_qhull_layer_' + str(layer_index)
+f = open(file_name, 'r')
+lines = f.readlines()
+
+cur_dim = int(lines[0])
+cur_card = int(lines[1])
+data_norm_list = []
+data_list = []
+for kk in range(cur_card):
+    current_data_record = np.fromstring(lines[kk + 2], dtype=float, sep=' ')
+    current_data_record = np.asarray(current_data_record)
+    data_list.append(current_data_record)
+    temp_norm = np.linalg.norm(current_data_record)
+    # data_norm_list.append(format(temp_norm, '.5f'))
+    data_norm_list.append(float("{0:.5f}".format(temp_norm)))
+
+f.close()
+data_norm_list = np.asarray(data_norm_list)
+
+
+
+
+
+
+
+
+
+# query_file_name = query_folder + 'query_' + str(dimension) + 'D.txt'
+# f = open(query_file_name, 'r')
+# lines = f.readlines()
+# cur_dim = int(lines[0])
+# cur_card = int(lines[1])
+# query_list = []
+# for kk in range(cur_card):
+#     current_query_record = np.fromstring(lines[kk + 2], dtype=float, sep=' ')
+#     current_query_record = np.asarray(current_query_record)
+#     # temp_norm = np.linalg.norm(current_data_record)
+#     query_list.append(current_query_record)
+# f.close()
+# query_list = np.asarray(query_list)
+
+# ==================== load data into bin ====================
+min_norm = min(data_norm_list)
+max_norm = max(data_norm_list)
+
+norm_range = max_norm - min_norm
+bin_size = norm_range / chunks
+
+bin_array = []
+cur_norm = min_norm
+
+float("{0:.2f}".format(max_norm))
+
+while float("{0:.5f}".format(cur_norm)) <= float("{0:.5f}".format(max_norm)):
+    bin_array.append(cur_norm)
+    cur_norm = cur_norm + bin_size
+
+print(bin_array)
+print(bin_array.__len__())
+bins = np.array(bin_array)
+
+bin_count_array = np.digitize(data_norm_list, bins)
+
+total_counter = Counter(bin_count_array)
+sorted_counter = sorted(total_counter.items())
+# plt.bar(total_counter.keys(), total_counter.values())
+print(total_counter)
+print(sum(total_counter.values()))
+print(sorted_counter)
+print(sorted_counter.__len__())
+
+for ii in range(sorted_counter.__len__()):
+    print(str(sorted_counter[ii][0]) + ' ' + str(sorted_counter[ii][1]) )
+
+weight = []
+
+
+print('Done')
+
+# for ii in range(cardinality):
+#     # cur_query = query_list[ii]
+#     inner_prod_list = []
+#     # temp_data_norm_list = data_norm_list
+#     # temp_data_norm_list = np.asarray(temp_data_norm_list)
+#     for jj in range(cardinality):
+#         cur_data = data_list[jj]
+#         # temp_dot_product = dot(cur_data, cur_query)
+#         inner_prod_list.append(temp_dot_product)
+#     inner_prod_list = np.asarray(inner_prod_list)
+#     reverse_sort_index = np.argsort((-inner_prod_list))
+#     top_k_index = reverse_sort_index[0: top_k]
+#
+#     selected_norms = data_norm_list[list(top_k_index)]
+#     selected_inner_prod = inner_prod_list[list(top_k_index)]
+#     bin_count_array = np.digitize(selected_norms, bins)
+#     temp_counter = Counter(bin_count_array)
+#     total_counter = total_counter + temp_counter
+
+# cur_bins = np.histogram(data_norm_list, bins)
+# bins_count = cur_bins[0]
+# bins_range = cur_bins[1]
+# print(bins_count)
+# plt.hist(norm_list, normed=True, bins=30)
+# plt.ylabel('Probability')
+
+# # ==================== check top-25 how many elements in which bin ====================
+# total_counter = Counter()
+# for ii in range(query_num):
+#     # cur_query = query_list[ii]
+#     inner_prod_list = []
+#     # temp_data_norm_list = data_norm_list
+#     # temp_data_norm_list = np.asarray(temp_data_norm_list)
+#     for jj in range(cardinality):
+#         cur_data = data_list[jj]
+#         # temp_dot_product = dot(cur_data, cur_query)
+#         inner_prod_list.append(temp_dot_product)
+#     inner_prod_list = np.asarray(inner_prod_list)
+#     reverse_sort_index = np.argsort((-inner_prod_list))
+#     top_k_index = reverse_sort_index[0: top_k]
+#
+#     selected_norms = data_norm_list[list(top_k_index)]
+#     selected_inner_prod = inner_prod_list[list(top_k_index)]
+#     bin_count_array = np.digitize(selected_norms, bins)
+#     temp_counter = Counter(bin_count_array)
+#     total_counter = total_counter + temp_counter
+#
+# # plt.bar(total_counter.keys(), total_counter.values())
+
--- Python_Analysis/Test.py
+++ Python_Analysis/Test.py
@@ -4,105 +4,19 @@ import numpy as np
 #
 # yy = np.std(x)
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 from  collections import Counter
-a = [1,1,1,1,2,2,2,2,3,3,4,5,5]
-b = [1,1,1,1,2,2,2,2,3,3,4,5,5]
-counter_c = Counter()
-counter_a = Counter(a)
-counter_b = Counter(a)
-
-counter_c = counter_c + counter_b
-counter_c = counter_c + counter_a
-print(counter_c)
-
-plt.bar(counter_c.keys(), counter_c.values())
-# print(type(counter))
-# # Counter({1: 4, 2: 4, 3: 2, 5: 2, 4: 1})
-# print(counter.values())
-# # [4, 4, 2, 1, 2]
-# print(counter.keys())
-# # [1, 2, 3, 4, 5]
-
-
-def dot(K, L):
-   if len(K) != len(L):
-      return 0
-
-   return sum(i[0] * i[1] for i in zip(K, L))
 
 
 
 
 
-data = [16 ,   78  , 251 ,  564  ,1121 , 1953,  3062,  4454 , 6330 , 8753, 11458, 15129, 18963,
-        21103, 22608, 21815, 19633, 16395, 11703,  7625,  4206,  1868,   707,   173, 32]
 
-#
-# my_list = [3,2,56,4,32,4,7,88,4,3,4, 100]
-# my_list = np.asarray(my_list)
-# ooo = np.argsort((-my_list))
-# print(ooo)
-# print(list(ooo))
-# print(type(ooo))
-# print(type(list(ooo)))
-# print(my_list[list(ooo)])
-# bins = [0, 20, 40, 60, 80, 100]
-# ttt = np.digitize(my_list, bins)
-# print(ttt)
-#
-# arr = np.array([10, 20, 30, 40, 50])
-# idx = [1, 0, 3, 4, 2]
-# print(arr[idx])
-# print(type(idx))
-#
-#
-# query = [0.1, 0.2, 0.3, 0.4]
-# print(query)
-#
-# chunks = 25
-# top_k = 25
-# data_folder = '/Users/sicongliu/Desktop/StreamingTopK/H2_ALSH/raw_data/Synthetic/'
-# query_folder = '/Users/sicongliu/Desktop/StreamingTopK/H2_ALSH/query/'
-# data_type = 'random_'
-# dimension = 4
-# cardinality = 200000
-#
-# file_name = data_folder + data_type + str(dimension) + '_' + str(cardinality) + '.txt'
-# f = open(file_name, 'r')
-# lines = f.readlines()
-#
-# cur_dim = int(lines[0])
-# cur_card = int(lines[1])
-# norm_list = []
-# data_list = []
-# for kk in range(cur_card):
-#     current_data_record = np.fromstring(lines[kk + 2], dtype=float, sep=' ')
-#     current_data_record = np.asarray(current_data_record)
-#     data_list.append(current_data_record)
-#     temp_norm = np.linalg.norm(current_data_record)
-#     norm_list.append(temp_norm)
-# f.close()
-# norm_list = np.asarray(norm_list)
-#
-# inner_prod_list = []
-# for jj in range(data_list.__len__()):
-#     data_vector = data_list[jj]
-#     inner_prod = dot(data_vector, query)
-#     inner_prod_list.append(inner_prod)
-#
-# # sort index based on the dot product, then check which bin norm falls into
-# inner_prod_list = np.asarray(inner_prod_list)
-# reverse_sort_index = np.argsort((-inner_prod_list))
-# top_k_index = reverse_sort_index[0: top_k]
-#
-# selected_norms = norm_list[list(top_k_index)]
-# selected_inner_prod = inner_prod_list[list(top_k_index)]
-# # match norm value into bin
-#
-# print(selected_norms)
-# print(selected_inner_prod)
-# print("Done")
-#
-#
+# data = [16 ,   78  , 251 ,  564  ,1121 , 1953,  3062,  4454 , 6330 , 8753, 11458, 15129, 18963,
+#         21103, 22608, 21815, 19633, 16395, 11703,  7625,  4206,  1868,   707,   173, 32]
+data = [4.5701172347699135, 4.66231147309165, 4.754505711413387, 4.8466999497351235, 4.93889418805686, 5.031088426378597,
+        5.1232826647003336, 5.21547690302207, 5.307671141343807, 5.399865379665544, 5.49205961798728, 5.584253856309017,
+        .676448094630754, 5.76864233295249, 5.860836571274227, 5.953030809595964, 6.0452250479177, 6.137419286239437,
+        6.229613524561174, 6.32180776288291, 6.414002001204647, 6.506196239526384, 6.59839047784812, 6.690584716169857, 6.782778954491594, 6.87497319281333]
+print(data.__len__())
