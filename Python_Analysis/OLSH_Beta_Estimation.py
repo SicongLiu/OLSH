b3c733424b95485bdf2807dac818a1d0542b894d
sicongliu <sliu104@asu.edu>
2020-02-11 00:22:48 -0700

    OLSH beta estimation

--- Python_Analysis/OLSH_Beta_Estimation.py
+++ Python_Analysis/OLSH_Beta_Estimation.py
@@ -7,6 +7,10 @@ import matplotlib.pyplot as plt
 from collections import Counter
 import math
 from scipy.stats import beta
+import scipy.stats as stats
+
+
+# def plot_beta(alpha_, beta_):
 
 
 def load_query(query_folder_, dimension_, is_stats_learn_):
@@ -30,7 +34,7 @@ def load_query(query_folder_, dimension_, is_stats_learn_):
 def calc_ground_truth(query_data_, query_count_, data_, topk):
     ground_truth_dot = []
     for ii in range(query_count_):
-        print("Query index: " + str(ii))
+        # print("Query index: " + str(ii))
         cur_query = query_data_[ii]
         cur_query = np.asarray(cur_query)
         cur_ground_truth_dot = calc_dot_rank(cur_query, data_, topk)
@@ -73,23 +77,29 @@ def calc_true_recall(val_list_, ground_truth_, topk):
     return float(temp_index_ + 1)/float(topk)
 
 
-def calc_layer_ratio(qhull_folder_, data_type_, dim_, card_, topk, query_, query_count_, total_qhull_layer_, ground_truth_dot_):
+def calc_layer_ratio(qhull_folder_, data_type_, dim_, card_, total_layer_, topk, query_, query_count_, total_qhull_layer_, ground_truth_dot_):
     topk = min(topk, total_qhull_layer_)
     recall_ratio_ = []
-    data_ratio_ = []
+    indexed_data_ = []
     for ii in range(topk):
-        cur_qhull_file = qhull_folder_ + '_' + data_type_ + '_' + str(dim_) + '_' + str(card_) + '_qhull_layer_' + str(ii)
+        cur_qhull_file = qhull_folder_ + data_type_ + '_' + str(dim_) + '_' + str(card_) + '_qhull_layer_' + str(ii)
         cur_qhull_data_ = load_data(cur_qhull_file, dim_)
-
-        layer_card_ratio = float(cur_qhull_data_.__len__())/float(card_)
-        data_ratio_.append(layer_card_ratio)
+        indexed_data_.append(cur_qhull_data_.__len__())
+        # layer_card_ratio = float(cur_qhull_data_.__len__())/float(card_)
         layer_recall_ratio = []
         for jj in range(query_count_):
             cur_query = query_[jj]
             temp_dot = calc_dot_rank(cur_query, cur_qhull_data_, topk)
-            temp_ratio = calc_true_recall(temp_dot, ground_truth_dot_)
+            # need to be adapted to the layer elements
+            while len(temp_dot) < topk:
+                temp_dot = np.append(temp_dot, -1.0)
+            temp_ratio = calc_true_recall(temp_dot, ground_truth_dot_[jj], topk)
             layer_recall_ratio.append(temp_ratio)
         recall_ratio_.append(np.mean(layer_recall_ratio))
+    indexed_data_ = np.asarray(indexed_data_)
+    total_indexed_data_ = np.sum(indexed_data_)
+    # data_ratio_ = float(indexed_data_)/float(total_indexed_data_)
+    data_ratio_ = np.divide(indexed_data_, total_indexed_data_)
     return recall_ratio_, data_ratio_
 
 
@@ -105,41 +115,75 @@ def compute_alpha_beta(input_ndarray_, min_val_, max_val_):
 
 
 def calc_layer_weight(alpha_, beta_, data_ratio_):
-    layer_weight = []
-    for ii in range(len(data_ratio_)):
-
+    beta_weight = []
+    for ii in range(recall_ratio.__len__()):
         if ii == 0:
-            cur_data_ratio = data_ratio_[ii]
-            cur_weight = beta.cdf(cur_data_ratio, alpha_, beta_, loc=0, scale=1)
+            cur_weight = beta.cdf(ii, alpha_, beta_, loc=1, scale=recall_ratio.__len__())
         else:
-            cur_data_ratio = data_ratio_[ii]
-            pre_data_ratio = data_ratio_[ii - 1]
-            cur_weight = beta.cdf(cur_data_ratio, alpha_, beta_, loc=0, scale=1) - beta.cdf(pre_data_ratio, alpha_, beta_, loc=0, scale=1)
-        layer_weight.append(cur_weight)
-    return layer_weight
+            t1 = beta.cdf(ii, alpha_, beta_, loc=1, scale=recall_ratio.__len__())
+            t2 = beta.cdf(ii + 1, alpha_, beta_, loc=1, scale=recall_ratio.__len__())
+            cur_weight = t2 - t1
+            beta_weight.append(cur_weight)
+    beta_weight.append(0)
+    return beta_weight
 
 
-query_folder = '../H2_ALSH/qhull_data/Synthetic/'
+qhull_folder = '../H2_ALSH/qhull_data/Synthetic/'
+query_folder = '../H2_ALSH/query/'
 raw_data_folder = '../H2_ALSH/raw_data/Synthetic/'
 is_stats_learn = 1
-dim = 2
-card = 200000
+# dims = [2, 3, 4, 5]
+# dims = [6, 7]
+dims = [4]
+# cards = [200000]
+cards = [500000, 1000000, 1500000, 2000000]
 topk = 25
-min_val = 0
-max_val = 1
-data_type = ['anti_correlated', 'correlated', 'random']
-query_list = load_query(query_folder, dim, is_stats_learn)
-data_list = load_data(data_file, dim)
-query_count = len(query_list)
-ground_truth_dot = calc_ground_truth(query_list, query_count, data_list, topk)
 
+data_types = ['anti_correlated', 'correlated', 'random']
 
-recall_ratio, data_ratio = calc_layer_ratio(qhull_folder_, data_type_, dim_, card_, topk, query_, query_count_, total_qhull_layer_,
-                     ground_truth_dot_)
-alpha, beta = compute_alpha_beta(recall_ratio, min_val, max_val)
-layer_wegith = calc_layer_weight(alpha, beta, data_ratio)
+# data_types = ['correlated']
+total_qhull_layer = topk
+for dim in dims:
+    query_list = load_query(query_folder, dim, is_stats_learn)
+    for card in cards:
+        for data_type in data_types:
+            if dim == 6:
+                if data_type == 'anti_correlated':
+                    total_qhull_layer = 21
+                elif data_type == 'correlated':
+                    total_qhull_layer = 25
+                else:
+                    total_qhull_layer = 20
+            elif dim == 7:
+                if data_type == 'anti_correlated':
+                    total_qhull_layer = 14
+                elif data_type == 'correlated':
+                    total_qhull_layer = 17
+                else:
+                    total_qhull_layer = 13
 
+            data_file = raw_data_folder + data_type + '_' + str(dim) + '_' + str(card) + '.txt'
+            data_list = load_data(data_file, dim)
+            query_count = len(query_list)
+            ground_truth_dot = calc_ground_truth(query_list, query_count, data_list, topk)
+            recall_ratio, data_ratio = calc_layer_ratio(qhull_folder, data_type, dim, card, total_qhull_layer, topk, query_list, query_count, total_qhull_layer,
+                             ground_truth_dot)
+            recall_ratio = np.asarray(recall_ratio)
+            if sum(recall_ratio) > 1:
+                recall_ratio = np.divide(recall_ratio, sum(recall_ratio))
+            range_ = np.arange(recall_ratio.__len__())
+            sample_stats = stats.rv_discrete(name='sample_nums', values=(range_, recall_ratio))
+            sample_values_ = sample_stats.rvs(size=100000)
+            # tt = Counter(sample_values_)
+            mc = np.digitize(sample_values_, range_)
+            alpha_, beta_ = compute_alpha_beta(mc, 1, recall_ratio.__len__())
 
+            # print('recall ratio: ', recall_ratio, ', alpha: ', alpha_, ', beta: ', beta_)
+            layer_weight = calc_layer_weight(alpha_, beta_, data_ratio)
+            layer_weight = np.asarray(layer_weight)
+            # np.set_printoptions(suppress=True)
+            yy = np.array2string(layer_weight, precision=5, separator=',', suppress_small=True)
+            print('dim: ', dim, ', data type: ', data_type, 'beta weight: ', yy, '\n\n')
 print('Done')
 
 
